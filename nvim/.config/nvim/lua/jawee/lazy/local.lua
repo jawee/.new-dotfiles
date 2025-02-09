cc6bc9d1eabeca846b87f36ba260f2f03ac322a5
Andreas Olsson <olsson.andreas@gmail.com>
2025-02-09 14:46:30 +0100

    azuredo update

--- nvim/.config/nvim/lua/jawee/lazy/local.lua
+++ nvim/.config/nvim/lua/jawee/lazy/local.lua
@@ -1,18 +1,11 @@
 local local_plugins = {
     {
         "jawee/azuredo.nvim",
+        -- dir = "~/projects/azuredo.nvim",
         config = function()
-            require("azuredo").setup()
+        require("azuredo").setup({ project = 'jawee', debug = false, telescope = true, fidget = true })
             vim.keymap.set("n", "<leader>az", "<cmd>Azuredo<CR>")
         end,
-        -- "azuredo.nvim",
-        -- dir = "~/projects/azuredo.nvim",
-        -- config = function()
-        --     require("azuredo").setup()
-        --     -- vim.keymap.set("n", "<leader>ct", "<cmd>CockpitTest<CR>")
-        --     -- vim.keymap.set("n", "<leader>cr", "<cmd>CockpitRefresh<CR>")
-        --     vim.keymap.set("n", "<leader>az", "<cmd>Azuredo<CR>")
-        -- end,
     }
 }
 
