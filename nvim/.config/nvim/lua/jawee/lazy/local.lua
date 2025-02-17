dc132fea876e3a90595779548c187ecccfcea1af
Andreas Olsson <olsson.andreas@gmail.com>
2025-02-17 16:23:35 +0100

    local.lua

--- nvim/.config/nvim/lua/jawee/lazy/local.lua
+++ nvim/.config/nvim/lua/jawee/lazy/local.lua
@@ -2,9 +2,21 @@ local local_plugins = {
     {
         "jawee/azuredo.nvim",
         -- dir = "~/projects/azuredo.nvim",
+        dependencies = {
+            'nvim-telescope/telescope.nvim',
+            'j-hui/fidget.nvim',
+        },
         config = function()
-        require("azuredo").setup({ project = 'jawee', debug = false, telescope = true, fidget = true })
+            require("azuredo").setup({ project = 'jawee', debug = false, telescope = true, fidget = true })
             vim.keymap.set("n", "<leader>az", "<cmd>Azuredo<CR>")
+        end,
+    },
+    {
+        "jawee/tmuxcmd.nvim",
+        -- dir = "~/projects/tmuxcmd.nvim",
+        config = function()
+            require("tmuxcmd").setup({})
+            vim.keymap.set("n", "<leader>tm", "<cmd>Tmuxcmd<CR>")
         end,
     }
 }
