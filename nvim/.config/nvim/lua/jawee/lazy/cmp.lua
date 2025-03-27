53165f309d005853004f7413bcec2d9c44a73317
Andreas Olsson <olsson.andreas@gmail.com>
2025-03-27 09:41:22 +0100

    add icons to cmp

--- nvim/.config/nvim/lua/jawee/lazy/cmp.lua
+++ nvim/.config/nvim/lua/jawee/lazy/cmp.lua
@@ -1,71 +1,70 @@
 return {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/cmp-nvim-lsp',
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-    },
-    config = function()
-        local cmp = require("cmp")
-        local source_mapping = {
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-nvim-lsp',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+    'onsails/lspkind.nvim',
+  },
+  config = function()
+    local lspkind = require('lspkind')
+    local cmp = require("cmp")
+
+    local select_opts = { behavior = cmp.SelectBehavior.Select }
+
+    require("luasnip.loaders.from_vscode").lazy_load()
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      mapping = {
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+        ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+      },
+      formatting = {
+        expandable_indicator = true,
+        fields = { 'abbr', 'kind', 'menu' },
+        format = lspkind.cmp_format({
+          mode = "symbol_text",
+          menu = ({
             buffer = "[Buffer]",
             nvim_lsp = "[LSP]",
+            luasnip = "[LuaSnip]",
             nvim_lua = "[Lua]",
             path = "[Path]",
-        }
+          })
+        }),
+      },
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+        { name = 'buffer' },
+        { name = 'path' },
+      })
+    })
 
-        local select_opts = { behavior = cmp.SelectBehavior.Select }
+    cmp.setup.cmdline('/', {
+      sources = {
+        { name = 'buffer' }
+      }
+    })
 
-        require("luasnip.loaders.from_vscode").lazy_load()
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = {
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ['<CR>'] = cmp.mapping.confirm({ select = false }),
-                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
-                ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
-            },
-            formatting = {
-                expandable_indicator = true,
-                fields = { 'abbr', 'kind', 'menu' },
-                format = function(entry, vim_item)
-                    local menu = source_mapping[entry.source.name]
-                    vim_item.menu = menu
-                    return vim_item
-                end,
-            },
-
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-                { name = 'buffer' },
-                { name = 'path' },
-            })
-        })
-
-        cmp.setup.cmdline('/', {
-            sources = {
-                { name = 'buffer' }
-            }
-        })
-
-        cmp.setup.cmdline(':', {
-            sources = cmp.config.sources({
-                { name = 'path' }
-            }, {
-                { name = 'cmdline' }
-            })
-        })
-
-    end
+    cmp.setup.cmdline(':', {
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
+      })
+    })
+  end
 }
