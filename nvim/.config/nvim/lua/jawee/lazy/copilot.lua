9e626fc0b7eacf095e319810cf7dabc5bcdf4b3a
Andreas Olsson <olsson.andreas@gmail.com>
2025-04-28 20:12:22 +0200

    copilot.lua

--- nvim/.config/nvim/lua/jawee/lazy/copilot.lua
+++ nvim/.config/nvim/lua/jawee/lazy/copilot.lua
@@ -1,14 +1,31 @@
 return {
-  'github/copilot.vim',
-  config = function()
-    vim.g.copilot_no_tab_map = true
-    vim.g.copilot_assume_mapped = true
-    vim.keymap.set('i', '<C-s>', 'copilot#Accept("\\<CR>")', {
-      expr = true,
-      replace_keycodes = false
-    })
-    vim.g.copilot_no_tab_map = true
-    vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#83a598" })
-    vim.api.nvim_set_hl(0, "CopilotAnnotation", { fg = "#83a598" })
-  end
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-s>",
+          },
+        },
+      })
+
+      local copilot = require("copilot.suggestion")
+      vim.keymap.set("i", "<C-s>", copilot.accept, {})
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    build = "make tiktoken",
+    opts = {
+    },
+  },
 }
