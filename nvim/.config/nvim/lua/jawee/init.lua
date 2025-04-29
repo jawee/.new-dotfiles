b81b9c286541a295c0895d06e5f3a148de66a3c0
Andreas Olsson <olsson.andreas@gmail.com>
2025-04-29 22:04:25 +0200

    vim.highlight deprecated

--- nvim/.config/nvim/lua/jawee/init.lua
+++ nvim/.config/nvim/lua/jawee/init.lua
@@ -7,7 +7,7 @@ require("jawee.custom")
 vim.api.nvim_create_autocmd('TextYankPost', {
     pattern = '*',
     callback = function()
-        vim.highlight.on_yank({
+        vim.hl.on_yank({
             higroup = 'IncSearch',
             timeout = 40,
         })
