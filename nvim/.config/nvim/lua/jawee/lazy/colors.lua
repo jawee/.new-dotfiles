64e7224692199f90ead1f9b7fd01cd3dc932b0f9
Andreas Olsson <olsson.andreas@gmail.com>
2025-05-20 14:34:48 +0200

    colors update

--- nvim/.config/nvim/lua/jawee/lazy/colors.lua
+++ nvim/.config/nvim/lua/jawee/lazy/colors.lua
@@ -1,52 +1,53 @@
 function SetColors(color)
-    color = color or "tokyonight"
-    vim.cmd.colorscheme(color)
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  color = color or "tokyonight"
+  vim.cmd.colorscheme(color)
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-    {
-        "folke/tokyonight.nvim",
-        name = "tokyonight",
-        config = function()
-            require('tokyonight').setup({
-                style = "storm",
-                transparent = true,
-                terminal_colors = true,
-                styles = {
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    sidebars = "dark",
-                    floats = "dark",
-                },
-            })
-            SetColors("tokyonight")
-        end
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        config = function()
-            require("gruvbox").setup({
-                dim_inactive = true,
-                transparent_mode = true,
-                contrast = "hard",
-                transparent = false,
-                terminal_colors = false,
-            })
-        end
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require("rose-pine").setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-        end
-    }
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    config = function()
+      require('tokyonight').setup({
+        style = "storm",
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+          sidebars = "dark",
+          floats = "dark",
+        },
+      })
+      SetColors("tokyonight")
+    end
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
+    config = function()
+      require("gruvbox").setup({
+        dim_inactive = true,
+        transparent_mode = true,
+        contrast = "hard",
+        transparent = false,
+        terminal_colors = false,
+      })
+    end
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        disable_background = true,
+        styles = {
+          italic = false,
+        },
+      })
+      SetColors("rose-pine-moon")
+    end
+  }
 }
