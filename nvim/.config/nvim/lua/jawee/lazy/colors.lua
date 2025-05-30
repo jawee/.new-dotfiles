c8ffcfc720022263bec564afe486fc234a9edc1c
Andreas Olsson <olsson.andreas@gmail.com>
2025-05-30 17:26:59 +0200

    feat: switch to light mode

--- nvim/.config/nvim/lua/jawee/lazy/colors.lua
+++ nvim/.config/nvim/lua/jawee/lazy/colors.lua
@@ -1,8 +1,12 @@
 function SetColors(color)
-  color = color or "tokyonight"
+  vim.o.background = "dark"
+  color = color or "rose-pine-moon"
   vim.cmd.colorscheme(color)
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+function SetLight()
+  vim.o.background = "light"
+  vim.cmd.colorscheme("rose-pine-dawn")
 end
 
 return {
@@ -21,7 +25,6 @@ return {
           floats = "dark",
         },
       })
-      SetColors("tokyonight")
     end
   },
   {
@@ -42,7 +45,6 @@ return {
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
-        disable_background = true,
         styles = {
           italic = false,
         },
