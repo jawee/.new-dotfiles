493da0c33dafbc3b201f1e62674e7a1f61e768f9
Andreas Olsson <olsson.andreas@gmail.com>
2026-04-07 18:11:40 +0200

    treesitter

--- nvim/.config/nvim/lua/jawee/lazy/treesitter.lua
+++ nvim/.config/nvim/lua/jawee/lazy/treesitter.lua
@@ -1,11 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   dependencies = {
     "nvim-treesitter/nvim-treesitter-context",
   },
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
+    require("nvim-treesitter").setup({
       -- A list of parser names, or "all"
       ensure_installed = {
         "vimdoc",
@@ -50,15 +52,15 @@ return {
       },
     })
 
-    local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    treesitter_parser_config.templ = {
-      install_info = {
-        url = "https://github.com/vrischmann/tree-sitter-templ.git",
-        files = { "src/parser.c", "src/scanner.c" },
-        branch = "master",
-      },
-    }
-
-    vim.treesitter.language.register("templ", "templ")
+    -- local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    -- treesitter_parser_config.templ = {
+    --   install_info = {
+    --     url = "https://github.com/vrischmann/tree-sitter-templ.git",
+    --     files = { "src/parser.c", "src/scanner.c" },
+    --     branch = "master",
+    --   },
+    -- }
+    --
+    -- vim.treesitter.language.register("templ", "templ")
   end,
 }
